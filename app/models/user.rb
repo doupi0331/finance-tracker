8cd1c20117e08ff38998c91835afc075fcaee09d
doupi0331 <lisau4mp6@gmail.com>
2017-01-11 13:41:34 +0800

    Add friendship and friend search functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,38 @@ class User < ActiveRecord::Base
     return false unless stock
     return user_stocks.where(stock_id: stock.id).exists?
   end
+  
+  def not_friends_with?(friend_id)
+    return friendships.where(friend_id: friend_id).count < 1
+  end
+  
+  def except_current_user(users)
+    users.reject {|user| user.id == self.id}
+  end
+  
+  def self.search(param)
+    return User.none if param.blank?
+    
+    param.strip! #清除空白
+    param.downcase!
+    
+    return (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+  end
+  
+  def self.first_name_matches(param)
+    return matches('first_name', param)
+  end
+  
+  def self.last_name_matches(param)
+    return matches('last_name', param)
+  end
+  
+  def self.email_matches(param)
+    return matches('email', param)
+  end
+  
+  def self.matches(field_name, param)
+    return where("lower(#{field_name}) like ?", "%#{param}%")
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   # post action之前, 先執行指定的controller
   devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
+  resources :users, only: [:show]
+  resources :friendships
   root "welcome#index"
   get "my_portfolio" => "users#my_portfolio"
   get "search_stock" => "stocks#search"
   get "my_friends" => "users#my_friends"
+  get "search_friends" => "users#search"
+  post "add_friend" => "users#add_friend"
 end
